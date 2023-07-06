a5b739b8999c795e0f9157c12c93f3e695575479
admin <mingaleev.d92@mail.ru>
2023-07-06 16:58:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkclient/ui/PostCard.kt
@@ -0,0 +1,16 @@
+package com.example.vkclient.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * @author : Mingaleev D
+ * @data : 06.07.2023
+ */
+
+
+@Preview
+@Composable
+fun PostCard() {
+
+}
