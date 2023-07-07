5311e0339c9b51b078fa7ccf7e7b9a49ecc57694
admin <mingaleev.d92@mail.ru>
2023-07-07 06:55:01 +0300

    демострационная карточка поста

--- app/src/main/java/com/example/vkclient/MainActivity.kt
+++ app/src/main/java/com/example/vkclient/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.vkclient.ui.PostCard
+import com.example.vkclient.ui.components.PostCard
 import com.example.vkclient.ui.theme.VKClientTheme
 
 class MainActivity : ComponentActivity() {
